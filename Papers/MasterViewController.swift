83d0671c1e5d9a580273c2032e690ea486f448a7
ArtmeLyksa <artemlyksa@gmail.com>
2016-06-10 16:19:07 +0300

    final commit

--- Papers/MasterViewController.swift
+++ Papers/MasterViewController.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2016 Razeware LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MasterViewController: UICollectionViewController {
+    
+    private var papersDataSource = PapersDataSource()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let width = CGRectGetWidth(collectionView!.frame)/3
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width) 
+    }
+    
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return papersDataSource.count;
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.textLabel.text = "\(indexPath.row)"
+        return cell;
+    }
+    
+}
